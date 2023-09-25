f65f32383f60e5eebffcaddc87de546e2161d711
samuel7qy <110116396+samuel7qy@users.noreply.github.com>
2023-09-25 11:45:23 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -8,21 +8,32 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
 //enquanto
-while (contador = listaDeTeclas.lenght) {
+for (let contador = 0; contador < listaDeTeclas.lenght; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-
-    console.log(instrumento);
-
-    const idAudio = `#som_{instrumento}`;
+    const idAudio = `#som_{instrumento}`; //template string
 
     tecla.onclick = function () {
-        tocaSom('#som_tecla_pom')
+        tocaSom(idAudio);   
     }
 
-    contador = contador + 1;
+    tecla.onkeydown = function (evento) {
+        tecla.classList.add('ativa');
 
-    console.log(contador)
+        console.log(evento.code == 'Space')
+
+        if (evento.code === 'Space') {
+            tecla.classList.add('ativa');
+        }
+
+        if (evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 
 }
